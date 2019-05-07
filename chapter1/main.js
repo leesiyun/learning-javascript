a427ede59f40316722643b899e892cb84eb2935a
Siyun Lee <siyun@Siyunui-MacBookPro.local>
2019-05-07 17:59:48 +0900

    Add chapter1

---
+++ chapter1/main.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+  "use strict";
+  paper.install(window);
+  paper.setup(document.getElementById("mainCanvas"));
+
+  const tool = new Tool();
+
+  const c = Shape.Circle(200, 200, 80);
+  c.fillColor = `black`;
+  const text = new PointText(200, 200);
+  text.justification = `center`;
+  text.fillColor = `white`;
+  text.fontSize = 20;
+  text.content = `hello world`;
+
+  tool.onMouseDown = function(event) {
+    const c = Shape.Circle(event.point, 20);
+    c.fillColor = `green`;
+  };
+
+  paper.view.draw();
+});
